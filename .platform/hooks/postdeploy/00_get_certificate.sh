73e67024ab4edea5051c74f21e12917fe6a6b4fa
bbundy12 <brendanbundy1@gmail.com>
2023-12-11 14:13:44 -0700

    Update security

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo certbot -n -d intexfall2023.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email jblosil1@byu.edu
-sudo certbot -n -d intexfall2023.is404.net --nginx --agree-tos --email jblosil1@byu.edu
+sudo certbot -n -d gitshopping.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email bmb242@byu.edu
+
