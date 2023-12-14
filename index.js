3f0fcce9d7cdccb0705e3d244ddfdf4acf0c1242
bbundy12 <brendanbundy1@gmail.com>
2023-12-13 19:59:00 -0700

    Small chnage

--- index.js
+++ index.js
@@ -19,17 +19,6 @@ app.use(fileUpload({
   limits: { fileSize: 50 * 1024 * 1024 },
 }));
 
-// const multer = require("multer");
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
-//   },
-// });
-// const upload = multer({ storage: storage });
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
