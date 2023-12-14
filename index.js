37fe214e8438752b696d49dc08250b2bb3a167c9
bbundy12 <brendanbundy1@gmail.com>
2023-12-13 21:49:22 -0700

    fix3

--- index.js
+++ index.js
@@ -390,7 +390,7 @@ app.post("/updateRecipe", async (req, res) => {
     });
 
 
-    es.redirect("/userLanding/" + user_id); // Redirect after successful update
+    res.redirect("/userLanding/" + user_id); // Redirect after successful update
   } catch (error) {
     console.error("Error updating recipe:", error);
     res.status(500).send("Internal Server Error");
