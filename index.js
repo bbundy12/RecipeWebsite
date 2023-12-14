5ad79be66bb303b8a75ea23cb3bb5d1e2f422247
JBlosil <150398479+JBlosil@users.noreply.github.com>
2023-12-13 20:24:52 -0700

    fixed

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ app.get("/userLanding/:user_id", async (req, res) => {
     const user_id = req.params.user_id;
     const recipes = await knex("recipes").where("user_id", user_id).select("title", "image");
 
-    res.render("userLanding", { recipes, user_id , image});
+    res.render("userLanding", { recipes, user_id});
   } catch (error) {
     console.error("Error fetching recipes:", error);
     res.status(500).send("Internal Server Error");
